dad2f3f1bc7a23483e2b5579ef4ae4bd276b76cd
Tom Ashworth <tashworth@twitter.com>
2013-10-18 16:20:53 +0100

    Further further reset.

--- js/devtoolsBackground.js
+++ js/devtoolsBackground.js
@@ -1,1 +1,7 @@
 var panels = chrome.devtools.panels;
+
+var panelAdded = function () {
+  console.log.apply(console, [].slice.call(arguments));
+};
+
+panels.create("Twitter Flight", "", "panel.html", panelAdded);
